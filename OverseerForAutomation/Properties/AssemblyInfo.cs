0a167d63ce26bc99de4ecfcf17ca1f09224b29d5
Rewati Jog <Rewati.Jog@TANDON.LOCAL>
2019-07-17 18:51:36 +0530

    Add project files.

---
+++ OverseerForAutomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OverseerForAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OverseerForAutomation")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a4fb9725-1f60-4ec9-8dae-30ec6f39f2e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OverseerForAutomation/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+ using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace OverseerForAutomation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver crmDriver = new ChromeDriver();
+            crmDriver.Navigate().GoToUrl("https://www.google.co.in");
+            IWebElement SearchTxt = crmDriver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input"));
+            SearchTxt.SendKeys("GitHub");
+            SearchTxt.SendKeys(Keys.Enter);
+            Thread.Sleep(2000);
+            crmDriver.Close();
+
+
+        }
+        [TestMethod]
+
+        public void TestMethod3()
+        {
+            IWebDriver crmDriver = new ChromeDriver();
+            crmDriver.Navigate().GoToUrl("https://www.google.co.in");
+            IWebElement SearchTxt = crmDriver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input"));
+            SearchTxt.SendKeys("GitHub2");
+            SearchTxt.SendKeys(Keys.Enter);
+            Thread.Sleep(2000);
+            crmDriver.Close();
+
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            IWebDriver crmDriver = new ChromeDriver();
+            crmDriver.Navigate().GoToUrl("https://www.google.co.in");
+            IWebElement SearchTxt = crmDriver.FindElement(By.XPath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input"));
+            SearchTxt.SendKeys("GitHub1");
+            SearchTxt.SendKeys(Keys.Enter);
+            Thread.Sleep(2000);
+            crmDriver.Close();
+
+
+        }
+    }
+}
